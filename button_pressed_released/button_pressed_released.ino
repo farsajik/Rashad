f48e1ddff06d20a911f42b7124af18892f58a0aa
farsajik <34785904+farsajik@users.noreply.github.com>
2017-12-29 11:51:10 +0400

    Add files via upload

---
+++ button_pressed_released/button_pressed_released.ino
@@ -0,0 +1,25 @@
+boolean butt_flag = 0;
+boolean butt;
+unsigned long last_press;
+
+void setup() {
+  pinMode(1, INPUT);
+  //Serial.begin(115200);
+}
+
+void loop() {
+/**********************************************************************/
+  butt = digitalRead(1); 
+  
+  if (butt == 1 && butt_flag == 0 && millis() - last_press > 100) {
+    butt_flag = 1;
+    //Serial.println("Button pressed");
+    last_press = millis();
+  }
+  if (butt == 0 && butt_flag == 1) {
+    butt_flag = 0;
+    //Serial.println("Button released");
+  }
+/**********************************************************************/
+
+}
